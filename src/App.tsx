a3760f66c6be2ab8a3bc43247db94317013a7bb8
HuyDinhUI <146050567+HuyDinhUI@users.noreply.github.com>
2025-07-27 18:58:09 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from './routes/publicRoutes';
+
+function App() {
+  
+
+  return (
+    <Router>
+      <div className='App'>
+        <Routes>
+          {publicRoutes.map((route,idx) => (
+            <Route key={idx} path={route.path} element={route.element}/>
+          ))}
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
